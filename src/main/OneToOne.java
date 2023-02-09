aac06ba35639ed72a5fb639e17726e4ee4cb4bd3
María <frm@fernandowirtz.com>
2023-02-09 12:42:00 +0100

    close

--- src/main/OneToOne.java
+++ src/main/OneToOne.java
@@ -9,18 +9,22 @@ import java.util.List;
 
 public class OneToOne {
 	public static void main(String[] args) {
-		
+
 		SessionFactory sessionFactory = SessionFactoryUtil.getSessionFactory();
 		Session session = sessionFactory.openSession();
-		
-		List<ContactInfo> infos = 
-				session.createQuery("SELECT p.contactInfos from Profesor p where p.id = :id")
-				//session.createQuery("SELECT c from ContactInfo c where c.profesor.id = :id")
-				.setParameter("id", 1).list();
-		
-		for (ContactInfo contactInfo : infos) {
-			System.out.println("contact info: " + contactInfo + " profesor: " + contactInfo.getProfesor());
+
+		{
+			List<ContactInfo> infos = session.createQuery("SELECT p.contactInfos from Profesor p where p.id = :id")
+					// session.createQuery("SELECT c from ContactInfo c where c.profesor.id = :id")
+					.setParameter("id", 1).list();
+
+			for (ContactInfo contactInfo : infos) {
+				System.out.println("contact info: " + contactInfo + " profesor: " + contactInfo.getProfesor());
+			}
 		}
+		
+		session.close();
+		sessionFactory.close();
 
 	}
 
